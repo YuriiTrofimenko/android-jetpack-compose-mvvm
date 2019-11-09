b227343f03e1200b7a500c649e53ee38c5125a93
YuriiTrofimenko <tyaa@ukr.net>
2019-11-09 10:38:19 +0200

    image as background

--- app/src/main/java/org/tyaa/jetpackcomposedemo1/MainActivity.kt
+++ app/src/main/java/org/tyaa/jetpackcomposedemo1/MainActivity.kt
@@ -1,15 +1,20 @@
 package org.tyaa.jetpackcomposedemo1
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
+import androidx.compose.unaryPlus
+import androidx.core.content.res.ResourcesCompat
 import androidx.ui.core.Text
 import androidx.ui.core.dp
 import androidx.ui.core.setContent
+import androidx.ui.foundation.DrawImage
 import androidx.ui.layout.Column
 import androidx.ui.layout.LayoutSize
 import androidx.ui.layout.Spacing
 import androidx.ui.material.MaterialTheme
+import androidx.ui.res.imageResource
 import androidx.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
@@ -23,11 +28,13 @@ class MainActivity : AppCompatActivity() {
 
 @Composable
 fun NewsStory() {
+    // Use 'mipmap' directory instead of 'drawable'!
+    val image = +imageResource(R.mipmap.header)
     Column(
         crossAxisSize = LayoutSize.Expand,
         modifier=Spacing(16.dp)
     ) {
-
+        DrawImage(image)
         Text("A day in Shark Fin Cove")
         Text("Davenport, California")
         Text("December 2018")
