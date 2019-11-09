a45d6ba7b808069edb367e2fe11d0382de207a65
YuriiTrofimenko <tyaa@ukr.net>
2019-11-09 09:26:45 +0200

    column-placement

--- app/src/main/java/org/tyaa/jetpackcomposedemo1/MainActivity.kt
+++ app/src/main/java/org/tyaa/jetpackcomposedemo1/MainActivity.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
 import androidx.ui.core.Text
+import androidx.ui.core.dp
 import androidx.ui.core.setContent
+import androidx.ui.layout.Column
+import androidx.ui.layout.LayoutSize
+import androidx.ui.layout.Spacing
 import androidx.ui.material.MaterialTheme
 import androidx.ui.tooling.preview.Preview
 
@@ -19,9 +23,15 @@ class MainActivity : AppCompatActivity() {
 
 @Composable
 fun NewsStory() {
-    Text("A day in Shark Fin Cove")
-    Text("Davenport, California")
-    Text("December 2018")
+    Column(
+        crossAxisSize = LayoutSize.Expand,
+        modifier=Spacing(16.dp)
+    ) {
+
+        Text("A day in Shark Fin Cove")
+        Text("Davenport, California")
+        Text("December 2018")
+    }
 }
 
 @Preview
